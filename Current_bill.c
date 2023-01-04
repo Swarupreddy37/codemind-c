20245d3319754533dee9b5de1a0bee9fe16de8b2
Swarupreddy37 <119949180+Swarupreddy37@users.noreply.github.com>
2023-01-04 11:17:56 +0530

    #include<stdio.h> int main() {     int u;     float a,b;     scanf("%d",&u);     if(u<=199)     {         b=(u*1.20);     }     else if(u>200 && u<=400)     {         b=(u*1.50);     }     else if(u>400 && u<=600)     {         b=(u*1.80);     }     else     {         b=(u*2.000);     }     if (b<=400) a=b+100;     else a=b+(0.15*b);     printf("%.2f",a); }

---
+++ Current_bill.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int u;
+    float a,b;
+    scanf("%d",&u);
+    if(u<=199)
+    {
+        b=(u*1.20);
+    }
+    else if(u>200 && u<=400)
+    {
+        b=(u*1.50);
+    }
+    else if(u>400 && u<=600)
+    {
+        b=(u*1.80);
+    }
+    else 
+    {
+        b=(u*2.000);
+    }
+    if (b<=400) a=b+100;
+    else a=b+(0.15*b);
+    printf("%.2f",a);
+}
