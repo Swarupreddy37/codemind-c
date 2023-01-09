0a1b678a7f12f36f754c1bd01e1aa29a5350dbf3
Swarupreddy37 <119949180+Swarupreddy37@users.noreply.github.com>
2023-01-09 14:27:04 +0530

    #include<stdio.h> int main() {     char C;     scanf("%c",&C);     if (C=='a')     printf("VOWEL");     else if (C=='e')     printf("VOWEL");     else if (C=='i')     printf("VOWEL");     else if (C=='o')     printf("VOWEL");     else if (C=='u')     printf("VOWEL");     else printf("CONSONANT"); }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char C;
+    scanf("%c",&C);
+    if (C=='a')
+    printf("VOWEL");
+    else if (C=='e')
+    printf("VOWEL");
+    else if (C=='i')
+    printf("VOWEL");
+    else if (C=='o')
+    printf("VOWEL");
+    else if (C=='u')
+    printf("VOWEL");
+    else printf("CONSONANT");
+}
